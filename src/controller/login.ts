5f780b3c49ae5f867b6855a94449efe01fbc8ddb
al-imam <alimam01828@gmail.com>
2023-04-14 03:01:51 +0600

    create login controller

---
+++ src/controller/login.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import userModel from "../models/userModel";
+import { compareSync } from "bcrypt";
+import plainUser from "../utilitys/getPlainUser";
+import jwt from "jsonwebtoken";
+
+async function login(req: Request, res: Response) {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if (user) {
+      if (compareSync(password, user.password)) {
+        return res.status(200).json({
+          user: plainUser(user),
+          access_token: jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET as string
+          ),
+        });
+      }
+    }
+
+    res.status(403).send("Authentication failed!");
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("something went wrong!");
+  }
+}
+
+export default login;
