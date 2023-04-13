0bbd5f21ab9d70fbebfe0d759985a49ee7cf4e3a
al-imam <alimam01828@gmail.com>
2023-04-14 02:28:33 +0600

    modify user object to send as response

--- src/controller/singup.ts
+++ src/controller/singup.ts
@@ -6,16 +6,30 @@ import { hashSync } from "bcrypt";
 async function singup(req: Request, res: Response) {
   try {
     const { email, password, picture } = req.body;
-    const { password: p, ...rest } = await userModel.create({
+    const user = await userModel.create({
       email,
       password: hashSync(password, 10),
       profile: picture ?? getRandomColor(email[0]),
     });
-    return res.status(201).json({ user: rest, token: "24" });
+
+    return res.status(201).json({
+      user: plainUser(user),
+      access_token: "24",
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).send("something went wrong!");
   }
+}
+
+function plainUser(object: any) {
+  return {
+    email: object.email,
+    profile: object.profile,
+    _id: object._id,
+    created: object.created_at,
+    updated: object.updated_at,
+  };
 }
 
 export default singup;
