9888e2ff835d5b75c16dbc59423f56451f6369f4
al-imam <alimam01828@gmail.com>
2023-04-13 23:53:53 +0600

    handle error in useExist middleware

--- src/middleware/userExist.ts
+++ src/middleware/userExist.ts
@@ -2,13 +2,17 @@ import { Request, Response, NextFunction } from "express";
 import userModel from "../models/userModel";
 
 async function userExist(req: Request, res: Response, next: NextFunction) {
-  const exist = await userModel.findOne({ email: req.body.email });
+  try {
+    const exist = await userModel.findOne({ email: req.body.email });
 
-  if (exist) {
-    res.status(403).send("user already exist!");
+    if (exist) {
+      return res.status(403).send("user already exist!");
+    }
+
+    next();
+  } catch (error) {
+    return res.status(500).send("something went wrong!");
   }
-
-  next();
 }
 
 export default userExist;
