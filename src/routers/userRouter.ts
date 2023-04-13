45e480e431ca7dd753441912987cc1de8fef4bed
al-imam <alimam01828@gmail.com>
2023-04-13 15:27:34 +0600

    catch error on singup route

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,23 +1,27 @@
 import express from "express";
 import userModel from "../models/userModel";
+import userExist from "../middleware/userExist";
+import asyncHandler from "express-async-handler";
 
 const userRouter = express.Router();
 
-userRouter.post("/", async (req, res) => {
-  const { email, password } = req.body;
+userRouter.post(
+  "/",
+  userExist,
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
 
-  const newUser = await userModel
-    .create({
-      email,
-      password,
-      profile: req.body?.picture,
-    })
-    .catch((e) => {
-      res.status(500).send("something went wrong");
-    });
+    const newUser = await userModel
+      .create({
+        email,
+        password,
+        profile: req.body?.picture,
+      })
+      .catch((e) => res.status(500).send("something went wrong!"));
 
-  res.status(201).json(newUser);
-});
+    res.status(201).json(newUser);
+  })
+);
 
 userRouter.post("/login", (req, res) => {
   res.send("Login successful!");
