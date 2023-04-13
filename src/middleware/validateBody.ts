c48d974ba22a1b9fc4e1a0fc3970ec810d5ebe94
al-imam <alimam01828@gmail.com>
2023-04-14 00:13:41 +0600

    create validateBody middleware

---
+++ src/middleware/validateBody.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+
+async function validateBody(req: Request, res: Response, next: NextFunction) {
+  try {
+    const { password, email } = req.body;
+
+    if (password === undefined || email === undefined) {
+      return res.status(403).send("Email and password not found");
+    }
+    return next();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("something went wrong!");
+  }
+}
+
+export default validateBody;
