83b81770323e036178b63dba6621a1362ac9a1a8
al-imam <alimam01828@gmail.com>
2023-04-14 00:21:51 +0600

    add timeStamps information

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,12 +1,15 @@
 import { Schema, model } from "mongoose";
 
-const userScheme = new Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  profile: {
-    type: Schema.Types.Mixed,
-    default: { fg: "#fff", bg: "#000", character: "A" },
+const userScheme = new Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profile: {
+      type: Schema.Types.Mixed,
+      default: { fg: "#fff", bg: "#000", character: "A" },
+    },
   },
-});
+  { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
+);
 
 export default model("User", userScheme);
