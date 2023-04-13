051c6fce52c12a954869ff6e0e85983eb1d03fe2
al-imam <alimam01828@gmail.com>
2023-04-14 00:22:05 +0600

    use singup controller

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,27 +1,11 @@
 import express from "express";
-import userModel from "../models/userModel";
 import userExist from "../middleware/userExist";
-import asyncHandler from "express-async-handler";
+import singup from "../controller/singup";
+import validateBody from "../middleware/validateBody";
 
 const userRouter = express.Router();
 
-userRouter.post(
-  "/",
-  userExist,
-  asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
-
-    const newUser = await userModel
-      .create({
-        email,
-        password,
-        profile: req.body?.picture,
-      })
-      .catch((e) => res.status(500).send("something went wrong!"));
-
-    res.status(201).json(newUser);
-  })
-);
+userRouter.post("/", validateBody, userExist, singup);
 
 userRouter.post("/login", (req, res) => {
   res.send("Login successful!");
