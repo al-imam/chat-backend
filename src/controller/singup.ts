59d76708a2644f69a7e6e4c333da10e04b1b315e
al-imam <alimam01828@gmail.com>
2023-04-14 02:00:00 +0600

    hash password

--- src/controller/singup.ts
+++ src/controller/singup.ts
@@ -1,16 +1,17 @@
 import { Request, Response } from "express";
 import userModel from "../models/userModel";
 import getRandomColor from "../utilitys/getRandomColor";
+import { hashSync } from "bcrypt";
 
 async function singup(req: Request, res: Response) {
   try {
     const { email, password, picture } = req.body;
-    const user = await userModel.create({
+    const { password: p, ...rest } = await userModel.create({
       email,
-      password,
+      password: hashSync(password, 10),
       profile: picture ?? getRandomColor(email[0]),
     });
-    return res.status(201).json(user);
+    return res.status(201).json({ user: rest, token: "24" });
   } catch (error) {
     console.log(error);
     return res.status(500).send("something went wrong!");
