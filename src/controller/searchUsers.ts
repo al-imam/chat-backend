5611fda047ec235be4fe6ae0dd0de98d0eef2561
al-imam <alimam01828@gmail.com>
2023-04-15 00:49:36 +0600

    create search user controller

---
+++ src/controller/searchUsers.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import userModel from "../models/userModel";
+
+function returnSearchQuery(search: string | undefined) {
+  if (!search) return {};
+  return {
+    email: { $regex: search, $options: "i" },
+  };
+}
+
+async function searchUser(req: Request, res: Response) {
+  try {
+    const users = await userModel
+      .find(returnSearchQuery(req.query.search as string))
+      .find({ _id: { $ne: req.body._user.id } })
+      .select("-password");
+
+    res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("something went wrong!");
+  }
+}
+
+export default searchUser;
