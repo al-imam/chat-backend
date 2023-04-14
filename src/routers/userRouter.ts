88125f0eaf5ca9fa480b82fa8232eb5b00548375
al-imam <alimam01828@gmail.com>
2023-04-15 00:51:33 +0600

    use searchUser controller on user/ route

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -3,8 +3,11 @@ import userExist from "../middleware/userExist";
 import singup from "../controller/singup";
 import validateBody from "../middleware/validateBody";
 import login from "../controller/login";
+import searchUser from "../controller/searchUsers";
 
 const userRouter = express.Router();
+
+userRouter.get("/", searchUser);
 
 userRouter.post("/singup", validateBody, userExist, singup);
 
