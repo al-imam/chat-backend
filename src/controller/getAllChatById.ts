5c6878c6827b67ec9d8f8953dcd124d1b8e55360
al-imam <alimam01828@gmail.com>
2023-04-15 23:53:15 +0600

    populate nested chat in array syntax

--- src/controller/getAllChatById.ts
+++ src/controller/getAllChatById.ts
@@ -3,14 +3,22 @@ import chatModel from "../models/chatModel";
 
 async function getAllChatById(req: Request, res: Response) {
   try {
-    const chat = await chatModel
-      .find({ users: { $elemMatch: { $eq: req.body._user._id } } })
-      .populate("users", "-password")
-      .populate("group_admin", "-password")
-      .populate("latest_message")
+    const chats = await chatModel
+      .find({
+        users: { $elemMatch: { $eq: req.body._user._id } },
+      })
+      .populate([
+        { path: "users", model: "User", select: "-password" },
+        { path: "group_admin", model: "User", select: "-password" },
+        {
+          path: "latest_message",
+          model: "Message",
+          populate: [{ path: "sender", model: "User", select: "-password" }],
+        },
+      ])
       .sort({ updated_at: -1 });
 
-    res.status(200).json(chat);
+    res.status(200).json(chats);
   } catch (error) {
     console.log({ error });
     return res.status(500).send("something went wrong!");
