b4bb53c3386406333b4a29b5ed18592379fa7e75
al-imam <alimam01828@gmail.com>
2023-04-15 19:45:21 +0600

    Fix bug in verify jwt token

--- src/middleware/filterAuthenticateUser.ts
+++ src/middleware/filterAuthenticateUser.ts
@@ -1,32 +1,31 @@
 import { Request, Response, NextFunction } from "express";
-import { verify } from "jsonwebtoken";
+import verifyJwtToken from "../utilitys/verifyJwtToken";
 import userModel from "../models/userModel";
 
 async function filterAuthenticateUser(
-  req: Request,
+  { headers: { authorization }, body }: Request,
   res: Response,
   next: NextFunction
 ) {
   try {
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith("Bearer ")
-    ) {
-      const token = req.headers.authorization.replace("Bearer ", "");
-
-      const { id } = verify(token, process.env.JWT_SECRET!) as { id: string };
-
-      if (!id) {
-        return res.status(401).send("Unauthorized");
-      }
-
-      req.body._user = await userModel.findById(id).select("-password");
-      return next();
+    if (!authorization || !authorization.startsWith("Bearer ")) {
+      return res.status(401).send("Unauthorized");
     }
+
+    const { id } = verifyJwtToken(authorization.replace("Bearer ", ""));
+
+    if (id !== null) {
+      const user = await userModel.findById(id).select("-password");
+      if (user) {
+        body._user = user;
+        return next();
+      }
+    }
+
     return res.status(401).send("Unauthorized");
   } catch (error) {
     console.log(error);
-    return res.status(500).send("something went wrong!");
+    return res.status(500).send("something went wrong!!!");
   }
 }
 
