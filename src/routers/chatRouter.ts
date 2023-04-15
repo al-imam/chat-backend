f3459674463b7d5ad92854d2f7dfa3c24a73db99
al-imam <alimam01828@gmail.com>
2023-04-15 20:20:58 +0600

    create /:userId get end point

--- src/routers/chatRouter.ts
+++ src/routers/chatRouter.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import filterAuthenticateUser from "../middleware/filterAuthenticateUser";
 import openChat from "../controller/openChat";
+import getAllChatByID from "../controller/getAllChatById";
 
 const chatRouter = express.Router();
 
 chatRouter.post("/:userId", filterAuthenticateUser, openChat);
+chatRouter.get("/:userId", filterAuthenticateUser, getAllChatByID);
 
 export default chatRouter;
