1cb62132c7b92c7276ebadfd3b3fffbd99c073f5
al-imam <alimam01828@gmail.com>
2023-04-15 15:49:09 +0600

    create openChat controller

---
+++ src/controller/openChat.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from "express";
+import chatModel from "../models/chatModel";
+import userModel from "../models/userModel";
+
+async function openChat(req: Request, res: Response) {
+  try {
+    const chats = await chatModel
+      .findOne({
+        is_group_chat: false,
+        $and: [
+          { users: { $elemMatch: { $eq: req.body._user._id } } },
+          { users: { $elemMatch: { $eq: req.params.userId } } },
+        ],
+      })
+      .populate("users", "-password")
+      .populate("latest_message");
+
+    const populate_chats = await userModel.populate(chats, {
+      path: "latest_message.sender",
+      select: "email profile",
+    });
+
+    console.log({ populate_chats });
+    if (populate_chats) {
+      return res.status(200).json(chats);
+    }
+
+    const newChat = (
+      await chatModel.create({
+        chat_name: "sender",
+        is_group_chat: false,
+        users: [req.body._user._id, req.params.userId],
+      })
+    ).populate("users", "-password");
+
+    return res.status(201).json(newChat);
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).send("something went wrong!");
+  }
+}
+
+export default openChat;
