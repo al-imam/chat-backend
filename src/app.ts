c45d3021efc2184e5ad40b3119ea2424eb9318fc
al-imam <alimam01828@gmail.com>
2023-04-15 15:49:31 +0600

    use chatRouter inside /chat

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import connectMongoDB from "./config/db";
 import userRouter from "./routers/userRouter";
+import chatRouter from "./routers/chatRouter";
 
 config();
 connectMongoDB(process.env.MONGODB_URL);
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/user", userRouter);
+app.use("/chat", chatRouter);
 
 app.get("/", (_, res) => {
   res.json({ payload: "Hello world" });
