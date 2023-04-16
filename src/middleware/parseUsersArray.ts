9c16de64ea01cc95e739a34b555ed6aa2cd942c8
al-imam <alimam01828@gmail.com>
2023-04-16 09:24:46 +0600

    Fix bug parse users in try catch

--- src/middleware/parseUsersArray.ts
+++ src/middleware/parseUsersArray.ts
@@ -1,26 +1,35 @@
 import { Request, Response, NextFunction } from "express";
 
-function parseUsersArray() {
-  return (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const userArray = JSON.parse(req.body.userArray);
-      if (Array.isArray(userArray) && userArray.length > 2) {
-        req.body._userArray = userArray;
-        return next();
-      }
+function parseUsersArray(req: Request, res: Response, next: NextFunction) {
+  try {
+    const userArray = parseJson(req.body.userArray);
 
-      return res.status(400).json({
-        code: "users-body-not-valid",
-        message: "more then two user required to create group",
-      });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({
-        code: "parse-users-array",
-        message: "Internal server error!",
-      });
+    if (userArray) {
+      req.body._userArray = userArray;
+      return next();
     }
-  };
+
+    return res.status(400).json({
+      code: "users—body—not—valid",
+      message: "more then two user required to create group",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      code: "parse-users-array",
+      message: "Internal server error!",
+    });
+  }
+}
+
+function parseJson(array: string): string[] | null {
+  try {
+    const arr = JSON.parse(array);
+    if (!Array.isArray(arr) || arr.length <= 2) return null;
+    return arr;
+  } catch {
+    return null;
+  }
 }
 
 export default parseUsersArray;
