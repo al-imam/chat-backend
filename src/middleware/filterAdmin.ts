9f87cd0af93261f64fb369d2feeac6547c53d045
al-imam <alimam01828@gmail.com>
2023-04-17 15:39:46 +0600

    modify filterAdmin middleware fix bug invalid id

--- src/middleware/filterAdmin.ts
+++ src/middleware/filterAdmin.ts
@@ -5,17 +5,26 @@ async function filterAdmin(req: Request, res: Response, next: NextFunction) {
   try {
     const exist = await chatModel.findById(req.body.groupId);
 
-    if (exist && exist.group_admin?.equals(req.body._user._id)) return next();
+    if (!exist) {
+      return res.status(400).json({
+        code: "group-not-found",
+        message: "No ground by specified groupId",
+      });
+    }
 
-    return res.status(401).json({
-      code: "not-admin",
-      message: "only admin can modify users",
-    });
+    if (!exist.group_admin?.equals(req.body._user._id)) {
+      return res.status(401).json({
+        code: "not-admin",
+        message: "None admin can not modify group users",
+      });
+    }
+
+    return next();
   } catch (error) {
     console.log({ error });
-    return res.status(400).json({
-      code: "group-not-found",
-      message: "no group exist in specified groupId",
+    return res.status(500).json({
+      code: "filter-admin",
+      message: "Internal server error",
     });
   }
 }
