50d70973c5ae2854406ce43529771b55762aa696
al-imam <alimam01828@gmail.com>
2023-04-17 16:04:10 +0600

    Fix bug resolve request

--- src/utilitys/wrap.ts
+++ src/utilitys/wrap.ts
@@ -1,16 +1,18 @@
 import { Request, Response, NextFunction } from "express";
 
 function wrap(
-  callback: (req: Request, res: Response, next: NextFunction) => any,
+  callback: (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => Promise<void | Response<any, Record<string, any>>>,
   error = { code: "unknown", message: "Internal server error!" }
 ) {
   return (req: Request, res: Response, next: NextFunction) => {
-    try {
-      callback(req, res, next);
-    } catch (e) {
-      console.log({ e });
+    return Promise.resolve(callback(req, res, next)).catch((e) => {
+      console.log(e);
       res.status(500).json(error);
-    }
+    });
   };
 }
 
