1a0a066627f03d930b97340616c62c2701396314
al-imam <alimam01828@gmail.com>
2023-04-17 17:14:13 +0600

    wrap with wrap function

--- src/controller/renameGroup.ts
+++ src/controller/renameGroup.ts
@@ -1,40 +1,46 @@
 import { Request, Response } from "express";
 import chatModel from "../models/chatModel";
+import wrap from "../utilitys/wrap";
 
-async function renameGroup(req: Request, res: Response) {
-  try {
-    const { groupId, updatedGroupName } = req.body;
-    const group = await chatModel.findByIdAndUpdate(
-      groupId,
-      { chat_name: updatedGroupName },
-      { new: true }
-    );
+async function renameGroup(
+  { body: { groupId, updatedGroupName } }: Request,
+  res: Response
+) {
+  const updatedGroup = await chatModel.findByIdAndUpdate(
+    groupId,
+    { chat_name: updatedGroupName },
+    { new: true }
+  );
 
-    if (!group) {
-      return res.status(400).json({
-        code: "group-not-exist",
-        message: "can't find group by specified groupId",
-      });
-    }
-
-    const renamedGroup = await chatModel.findById(group._id).populate([
-      { path: "users", model: "User", select: "-password" },
-      { path: "group_admin", model: "User", select: "-password" },
-      {
-        path: "latest_message",
-        model: "Message",
-        populate: [{ path: "sender", model: "User", select: "-password" }],
-      },
-    ]);
-
-    return res.status(201).json(renamedGroup);
-  } catch (error) {
-    console.log({ error });
-    return res.status(500).json({
-      code: "rename-group",
-      message: "Internal server error!",
+  if (!updatedGroup) {
+    return res.status(400).json({
+      code: "group-not-exist",
+      message: "can't find group by specified groupId",
     });
   }
+
+  const populateGroup = await chatModel.findById(updatedGroup._id).populate([
+    {
+      path: "users",
+      model: "User",
+      select: "-password",
+    },
+    {
+      path: "group_admin",
+      model: "User",
+      select: "-password",
+    },
+    {
+      path: "latest_message",
+      model: "Message",
+      populate: [{ path: "sender", model: "User", select: "-password" }],
+    },
+  ]);
+
+  return res.status(200).json(populateGroup);
 }
 
-export default renameGroup;
+export default wrap(renameGroup, {
+  code: "rename-group",
+  message: "Internal server error!",
+});
