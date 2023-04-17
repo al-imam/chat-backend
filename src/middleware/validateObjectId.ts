3449a11035a3a90fae8db13b48ea32961512bb18
al-imam <alimam01828@gmail.com>
2023-04-17 16:44:33 +0600

    modify validateObjectId middleware
      - call next if array length is zero
      - else return invalid ids

--- src/middleware/validateObjectId.ts
+++ src/middleware/validateObjectId.ts
@@ -1,33 +1,29 @@
 import { Request, Response, NextFunction } from "express";
 import { Types } from "mongoose";
+import wrap from "../utilitys/wrap";
 
 function validateObjectId(properties: string[]) {
-  return (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const noneValidObjectIds: object[] = [];
+  const handler = async (req: Request, res: Response, next: NextFunction) => {
+    const noneValidObjectIds: Record<string, string>[] = [];
 
-      properties.forEach((property) => {
-        if (!Types.ObjectId.isValid(req.body[property])) {
-          noneValidObjectIds.push({ [property]: req.body[property] });
-        }
-      });
-
-      if (noneValidObjectIds.length > 0) {
-        return res.status(400).json({
-          message: "some object id not valid",
-          noneValidObjectIds,
-        });
+    properties.forEach((property) => {
+      if (!Types.ObjectId.isValid(req.body[property])) {
+        noneValidObjectIds.push({ [property]: req.body[property] });
       }
+    });
 
-      next();
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({
-        code: "validate-object-id",
-        message: "Internal server error!",
-      });
-    }
+    if (noneValidObjectIds.length === 0) return next();
+
+    return res.status(400).json({
+      message: "some object id not valid",
+      noneValidObjectIds,
+    });
   };
+
+  return wrap(handler, {
+    code: "validate-object-id",
+    message: "Internal server error",
+  });
 }
 
 export default validateObjectId;
