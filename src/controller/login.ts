aab183dd0fd53053e92f30f21324c421f8c90490
al-imam <alimam01828@gmail.com>
2023-04-17 16:54:58 +0600

    handle internal server errors in login controller

--- src/controller/login.ts
+++ src/controller/login.ts
@@ -2,26 +2,28 @@ import { Request, Response } from "express";
 import userModel from "../models/userModel";
 import { compareSync } from "bcrypt";
 import plainUser from "../utilitys/getPlainUser";
-import jwt from "jsonwebtoken";
+import { sign } from "jsonwebtoken";
+import wrap from "../utilitys/wrap";
 
 async function login(req: Request, res: Response) {
-  try {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const user = await userModel.findOne({ email });
+  const user = await userModel.findOne({ email });
 
-    if (user && compareSync(password, user.password)) {
-      return res.status(200).json({
-        user: plainUser(user),
-        access_token: jwt.sign({ id: user._id }, process.env.JWT_SECRET!),
-      });
-    }
-
-    res.status(403).send("email and password not match");
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send("something went wrong!");
+  if (user && compareSync(password, user.password)) {
+    return res.status(200).json({
+      user: plainUser(user),
+      access_token: sign({ id: user._id }, process.env.JWT_SECRET!),
+    });
   }
+
+  res.status(403).json({
+    code: "authentication-error",
+    message: "email and password not match",
+  });
 }
 
-export default login;
+export default wrap(login, {
+  code: "login-error",
+  message: "Internal server error",
+});
