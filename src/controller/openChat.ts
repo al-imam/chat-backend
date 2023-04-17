7c3f440a90547191de1773fcd7a91a4c3930dc3f
al-imam <alimam01828@gmail.com>
2023-04-17 17:04:16 +0600

    refactor openChat controller

--- src/controller/openChat.ts
+++ src/controller/openChat.ts
@@ -1,50 +1,64 @@
 import { Request, Response } from "express";
 import chatModel from "../models/chatModel";
-import userModel from "../models/userModel";
 
 async function openChat(req: Request, res: Response) {
-  try {
-    const openChat = await chatModel
-      .findOne({
-        is_group_chat: false,
-        $and: [
-          { users: { $elemMatch: { $eq: req.body._user._id } } },
-          { users: { $elemMatch: { $eq: req.body.id } } },
-        ],
-      })
-      .populate([
-        { path: "users", model: "User", select: "-password" },
-        {
-          path: "latest_message",
-          model: "Message",
-          populate: [{ path: "sender", model: "User", select: "-password" }],
-        },
-      ]);
-
-    if (openChat) {
-      return res.status(200).json(openChat);
-    }
-
-    const createChat = await chatModel.create({
-      chat_name: "sender",
+  const openChatByUsersId = await chatModel
+    .findOne({
       is_group_chat: false,
-      users: [req.body._user._id, req.body.id],
-    });
-
-    const fullChat = await chatModel.findById(createChat._id).populate([
-      { path: "users", model: "User", select: "-password" },
+      $and: [
+        { users: { $elemMatch: { $eq: req.body._user._id } } },
+        { users: { $elemMatch: { $eq: req.body.id } } },
+      ],
+    })
+    .populate([
+      {
+        path: "users",
+        model: "User",
+        select: "-password",
+      },
       {
         path: "latest_message",
         model: "Message",
-        populate: [{ path: "sender", model: "User", select: "-password" }],
+        populate: [
+          {
+            path: "sender",
+            model: "User",
+            select: "-password",
+          },
+        ],
       },
     ]);
 
-    return res.status(201).json(fullChat);
-  } catch (error) {
-    console.log({ error });
-    return res.status(500).send("something went wrong!");
+  if (openChatByUsersId) {
+    return res.status(200).json(openChatByUsersId);
   }
+
+  const createChat = await chatModel.create({
+    chat_name: "sender",
+    is_group_chat: false,
+    users: [req.body._user._id, req.body.id],
+  });
+
+  const populateNewChat = await chatModel.findById(createChat._id).populate([
+    {
+      path: "users",
+      model: "User",
+      select: "-password",
+    },
+    {
+      path: "latest_message",
+      model: "Message",
+      populate: [
+        {
+          path: "sender",
+          model: "User",
+          select: "-password",
+        },
+      ],
+    },
+  ]);
+
+  return res.status(201).json(populateNewChat);
 }
 
 export default openChat;
