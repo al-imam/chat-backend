41c592b7b840479ec494193ea29fdc252a9e64a5
al-imam <alimam01828@gmail.com>
2023-04-17 16:20:49 +0600

    abstract error handling with wrap function

--- src/middleware/filterAdmin.ts
+++ src/middleware/filterAdmin.ts
@@ -1,32 +1,28 @@
 import { Request, Response, NextFunction } from "express";
 import chatModel from "../models/chatModel";
+import wrap from "../utilitys/wrap";
 
 async function filterAdmin(req: Request, res: Response, next: NextFunction) {
-  try {
-    const exist = await chatModel.findById(req.body.groupId);
+  const exist = await chatModel.findById("eer");
 
-    if (!exist) {
-      return res.status(400).json({
-        code: "group-not-found",
-        message: "No ground by specified groupId",
-      });
-    }
-
-    if (!exist.group_admin?.equals(req.body._user._id)) {
-      return res.status(401).json({
-        code: "not-admin",
-        message: "None admin can not modify group users",
-      });
-    }
-
-    return next();
-  } catch (error) {
-    console.log({ error });
-    return res.status(500).json({
-      code: "filter-admin",
-      message: "Internal server error",
+  if (!exist) {
+    return res.status(400).json({
+      code: "group-not-found",
+      message: "No ground by specified groupId",
     });
   }
+
+  if (!exist.group_admin?.equals(req.body._user._id)) {
+    return res.status(401).json({
+      code: "not-admin",
+      message: "None admin can not modify group users",
+    });
+  }
+
+  return next();
 }
 
-export default filterAdmin;
+export default wrap(filterAdmin, {
+  code: "filter-admin",
+  message: "Internal server error",
+});
