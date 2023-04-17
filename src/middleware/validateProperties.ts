3d4f072372b2aaf6041f96a9a5847f70377f139e
al-imam <alimam01828@gmail.com>
2023-04-17 16:39:57 +0600

    handle Internal server error in validateProperties

--- src/middleware/validateProperties.ts
+++ src/middleware/validateProperties.ts
@@ -1,32 +1,28 @@
 import { Request, Response, NextFunction } from "express";
+import wrap from "../utilitys/wrap";
 
 function validateProperties(properties: string[]) {
-  return (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const missingDependency: string[] = [];
+  const handler = async (req: Request, res: Response, next: NextFunction) => {
+    const missingDependency: string[] = [];
 
-      properties.forEach((property) => {
-        if (!req.body.hasOwnProperty(property)) {
-          missingDependency.push(property);
-        }
-      });
-
-      if (missingDependency.length > 0) {
-        return res.status(400).json({
-          message: "some properties are missing in request body",
-          missingDependency,
-        });
+    properties.forEach((property) => {
+      if (!req.body.hasOwnProperty(property)) {
+        missingDependency.push(property);
       }
+    });
 
-      next();
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({
-        code: "validate-body",
-        message: "Internal server error!",
-      });
-    }
+    if (missingDependency.length === 0) return next();
+
+    return res.status(400).json({
+      message: "some properties are missing in request body",
+      missingDependency,
+    });
   };
+
+  return wrap(handler, {
+    code: "validate-body",
+    message: "Internal server error",
+  });
 }
 
 export default validateProperties;
