8f07b4b9a2e266211119a9ef7ae666eb9c192248
al-imam <alimam01828@gmail.com>
2023-04-17 17:09:49 +0600

    refactor addUserToGroup controller and handle error

--- src/controller/addUserToGroup.ts
+++ src/controller/addUserToGroup.ts
@@ -1,39 +1,43 @@
 import { Request, Response } from "express";
 import chatModel from "../models/chatModel";
+import wrap from "../utilitys/wrap";
 
 async function addUserToGroup(req: Request, res: Response) {
-  try {
-    const updatedUsers = await chatModel.findByIdAndUpdate(
-      req.body.groupId,
-      { $push: { users: req.body.userId } },
-      { new: true }
-    );
+  const updatedUsers = await chatModel.findByIdAndUpdate(
+    req.body.groupId,
+    { $push: { users: req.body.userId } },
+    { new: true }
+  );
 
-    if (!updatedUsers) {
-      return res.status(400).json({
-        code: "group-not-exist",
-        message: "can't find group by specified groupId",
-      });
-    }
-
-    const pc = await chatModel.findById(updatedUsers._id).populate([
-      { path: "users", model: "User", select: "-password" },
-      { path: "group_admin", model: "User", select: "-password" },
-      {
-        path: "latest_message",
-        model: "Message",
-        populate: [{ path: "sender", model: "User", select: "-password" }],
-      },
-    ]);
-
-    return res.status(200).json(pc);
-  } catch (error) {
-    console.log({ error });
-    return res.status(500).json({
-      code: "add-user-to-group",
-      message: "Internal server error!",
+  if (!updatedUsers) {
+    return res.status(400).json({
+      code: "group-not-exist",
+      message: "can't find group by specified groupId",
     });
   }
+
+  const userCollection = await chatModel.findById(updatedUsers._id).populate([
+    {
+      path: "users",
+      model: "User",
+      select: "-password",
+    },
+    {
+      path: "group_admin",
+      model: "User",
+      select: "-password",
+    },
+    {
+      path: "latest_message",
+      model: "Message",
+      populate: [{ path: "sender", model: "User", select: "-password" }],
+    },
+  ]);
+
+  return res.status(200).json(userCollection);
 }
 
-export default addUserToGroup;
+export default wrap(addUserToGroup, {
+  code: "add-user",
+  message: "Internal server error!",
+});
