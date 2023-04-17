1b110a6aab9a48df2d7b5315f7edb9577d0f9222
al-imam <alimam01828@gmail.com>
2023-04-17 16:27:20 +0600

    change error response on filterAuthenticateUser

--- src/middleware/filterAuthenticateUser.ts
+++ src/middleware/filterAuthenticateUser.ts
@@ -7,26 +7,27 @@ async function filterAuthenticateUser(
   res: Response,
   next: NextFunction
 ) {
-  try {
-    if (!authorization || !authorization.startsWith("Bearer ")) {
-      return res.status(401).send("Unauthorized");
-    }
-
-    const { id } = verifyJwtToken(authorization.replace("Bearer ", ""));
-
-    if (id !== null) {
-      const user = await userModel.findById(id).select("-password");
-      if (user) {
-        body._user = user;
-        return next();
-      }
-    }
-
-    return res.status(401).send("Unauthorized");
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send("something went wrong!!!");
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return res.status(401).json({
+      code: "authorization-headers-not-found",
+      message: "authorization headers is not valid or not sent",
+    });
   }
+
+  const { id } = verifyJwtToken(authorization.replace("Bearer ", ""));
+
+  if (id !== null) {
+    const user = await userModel.findById(id).select("-password");
+    if (user) {
+      body._user = user;
+      return next();
+    }
+  }
+
+  return res.status(401).json({
+    code: "authorization-headers-not-found",
+    message: "authorization headers is not valid or not sent",
+  });
 }
 
 export default filterAuthenticateUser;
