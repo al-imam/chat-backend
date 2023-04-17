63746702df8b692702f940ebc44c1226fd12459f
al-imam <alimam01828@gmail.com>
2023-04-17 16:22:10 +0600

    modify parseUsersArray to work with wrap function

--- src/middleware/parseUsersArray.ts
+++ src/middleware/parseUsersArray.ts
@@ -1,25 +1,22 @@
 import { Request, Response, NextFunction } from "express";
+import wrap from "../utilitys/wrap";
 
-function parseUsersArray(req: Request, res: Response, next: NextFunction) {
-  try {
-    const userArray = parseJson(req.body.userArray, req.body._user._id);
+async function parseUsersArray(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const userArray = parseJson(req.body.userArray, req.body._user._id);
 
-    if (userArray) {
-      req.body._userArray = userArray;
-      return next();
-    }
-
-    return res.status(400).json({
-      code: "users—body—not—valid",
-      message: "more then two user required to create group",
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      code: "parse-users-array",
-      message: "Internal server error!",
-    });
+  if (userArray) {
+    req.body._userArray = userArray;
+    return next();
   }
+
+  return res.status(400).json({
+    code: "users—body—not—valid",
+    message: "more then two user required to create group",
+  });
 }
 
 function parseJson(array: string, id: string): string[] | null {
@@ -33,4 +30,7 @@ function parseJson(array: string, id: string): string[] | null {
   }
 }
 
-export default parseUsersArray;
+export default wrap(parseUsersArray, {
+  code: "parse-users-array",
+  message: "Internal server error!",
+});
