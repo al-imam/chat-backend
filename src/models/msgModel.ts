56c5efda834de23b08b7092cc3086be211e03e4a
al-imam <alimam01828@gmail.com>
2023-04-18 19:03:30 +0600

    create message sending route an test it

--- src/models/msgModel.ts
+++ src/models/msgModel.ts
@@ -3,8 +3,8 @@ import { Schema, model } from "mongoose";
 const messageSchema = new Schema(
   {
     sender: { type: Schema.Types.ObjectId, ref: "User" },
-    content: { type: String, required: true },
-    chat: { type: Schema.Types.ObjectId, ref: "Chat" },
+    message: { type: String, required: true },
+    chat_id: { type: Schema.Types.ObjectId, ref: "Chat" },
     read_by: [{ type: Schema.Types.ObjectId, ref: "User" }],
   },
   { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
--- src/routers/messageRouter.ts
+++ src/routers/messageRouter.ts
@@ -1,5 +1,17 @@
 import express from "express";
+import validateProperties from "../middleware/validateProperties";
+import validateObjectId from "../middleware/validateObjectId";
+import filterAuthenticateUser from "../middleware/filterAuthenticateUser";
+import sendMessage from "../controller/sendMessage";
 
 const messageRouter = express.Router();
+
+messageRouter.post(
+  "/",
+  validateProperties(["chatId", "message"]),
+  validateObjectId(["chatId"]),
+  filterAuthenticateUser,
+  sendMessage
+);
 
 export default messageRouter;
