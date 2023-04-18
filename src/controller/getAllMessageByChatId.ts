521e4a24474e10b50bc9f28f0159e89e606332c5
al-imam <alimam01828@gmail.com>
2023-04-19 05:40:41 +0600

    sort order and implement limitToLast

--- src/controller/getAllMessageByChatId.ts
+++ src/controller/getAllMessageByChatId.ts
@@ -5,8 +5,9 @@ import msgModel from "../models/msgModel";
 async function getAllMessageByChatId(req: Request, res: Response) {
   const messageArray = await msgModel
     .find({ chat_id: req.body.chatId })
+    .sort({ created_at: "ascending" })
     .populate("sender", "email profile");
-  res.status(200).json(messageArray);
+  res.status(200).json(messageArray.slice(-20));
 }
 
 export default wrap(getAllMessageByChatId, {
