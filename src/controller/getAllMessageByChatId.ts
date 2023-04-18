a8f868d3034850bb7e84832e90634bdb220bc27b
al-imam <alimam01828@gmail.com>
2023-04-19 01:32:37 +0600

    populate find all chat query

--- src/controller/getAllMessageByChatId.ts
+++ src/controller/getAllMessageByChatId.ts
@@ -3,11 +3,13 @@ import wrap from "../utilitys/wrap";
 import msgModel from "../models/msgModel";
 
 async function getAllMessageByChatId(req: Request, res: Response) {
-  const messageArray = await msgModel.find({ chat_id: req.body.chatId });
+  const messageArray = await msgModel
+    .find({ chat_id: req.body.chatId })
+    .populate("sender", "email profile");
   res.status(200).json(messageArray);
 }
 
 export default wrap(getAllMessageByChatId, {
-  code: "send-message",
+  code: "get-message",
   message: "Internal server error",
 });
