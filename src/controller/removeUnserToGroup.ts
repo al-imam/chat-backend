051d287a6326cf37eeea55fcd2e36358acba8026
al-imam <alimam01828@gmail.com>
2023-04-18 11:11:07 +0600

    Fix bug in removeUser if user not exist throw bad request

--- src/controller/removeUnserToGroup.ts
+++ src/controller/removeUnserToGroup.ts
@@ -1,32 +1,35 @@
 import { Request, Response } from "express";
 import chatModel from "../models/chatModel";
+import wrap from "../utilitys/wrap";
 
 async function removeUserToGroup(req: Request, res: Response) {
-  try {
-    const removedUsers = await chatModel.findByIdAndUpdate(
-      req.body.groupId,
-      { $pull: { users: req.body.userId } },
-      { new: true }
-    );
+  const removedUsers = await chatModel.findByIdAndUpdate(
+    req.body.groupId,
+    { $pull: { users: req.body.userId } },
+    { new: true }
+  );
 
-    const pc = await chatModel.findById(removedUsers?._id).populate([
-      { path: "users", model: "User", select: "-password" },
-      { path: "group_admin", model: "User", select: "-password" },
-      {
-        path: "latest_message",
-        model: "Message",
-        populate: [{ path: "sender", model: "User", select: "-password" }],
-      },
-    ]);
-
-    return res.status(200).json(pc);
-  } catch (error) {
-    console.log({ error });
+  if (!removedUsers) {
     return res.status(400).json({
       code: "user-not-exist",
       message: "can not find user",
     });
   }
+
+  const pc = await chatModel.findById(removedUsers._id).populate([
+    { path: "users", model: "User", select: "-password" },
+    { path: "group_admin", model: "User", select: "-password" },
+    {
+      path: "latest_message",
+      model: "Message",
+      populate: [{ path: "sender", model: "User", select: "-password" }],
+    },
+  ]);
+
+  return res.status(200).json(pc);
 }
 
-export default removeUserToGroup;
+export default wrap(removeUserToGroup, {
+  code: "user-not-exist",
+  message: "can not find user",
+});
