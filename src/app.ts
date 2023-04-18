f13c1d175f7d85c374f8c2961d4880540623f593
al-imam <alimam01828@gmail.com>
2023-04-18 18:25:33 +0600

    setup new message router

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { config } from "dotenv";
 import connectMongoDB from "./config/db";
 import userRouter from "./routers/userRouter";
 import chatRouter from "./routers/chatRouter";
+import messageRouter from "./routers/messageRouter";
 
 config();
 connectMongoDB(process.env.MONGODB_URL);
@@ -10,12 +11,13 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/user", userRouter);
-app.use("/chat", chatRouter);
-
 app.get("/", (_, res) => {
   res.json({ payload: "Hello world" });
 });
+
+app.use("/user", userRouter);
+app.use("/chat", chatRouter);
+app.use("/message", messageRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`server is running on port ${process.env.PORT}!`);
